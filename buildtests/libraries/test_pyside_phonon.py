5f59c4ed3c55897b53f0c89682ef1958bc2625c2
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-02-02 23:59:23 +0000

    PySide phonon test. Not quite there yet though.

---
+++ buildtests/libraries/test_pyside_phonon.py
@@ -0,0 +1,34 @@
+import sys
+from PySide import QtCore, QtGui
+from PySide.phonon import Phonon
+
+def main():
+
+    app = QtGui.QApplication([])
+    app.setApplicationName('test_pyside_phonon')
+
+    print "Qt plugin paths: " + unicode(list(app.libraryPaths()))
+    print 'Qt Libraries path: ' + unicode(QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibrariesPath))
+
+    try:
+        media_source = Phonon.MediaSource('test_pyside_phonon.wav')
+
+        media_object = Phonon.MediaObject()
+        media_object.setCurrentSource(media_source)
+        media_object.finished.connect(app.quit)
+
+        audio_output = Phonon.AudioOutput(Phonon.NotificationCategory)
+
+        Phonon.createPath(media_object, audio_output)
+
+        media_object.play()
+    
+        status = app.exec_()
+
+    except:
+        status = 1
+
+    return status
+
+if __name__ == "__main__":
+    sys.exit(main())
