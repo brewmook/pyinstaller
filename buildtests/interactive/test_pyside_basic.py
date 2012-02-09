fb12ac40bc795768015b71514025d8f0fc7cbb44
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-02-09 23:44:42 +0000

    Add hook for PySide.QtGui

--- buildtests/interactive/test_pyside_basic.py
+++ buildtests/interactive/test_pyside_basic.py
@@ -3,16 +3,11 @@
 
 import sys
 
-from PySide import QtCore, QtGui
+from PySide.QtGui import QApplication, QLabel
 
 def main():
-    app = QtGui.QApplication(sys.argv)
-    formats = [unicode(format).lower() for format in QtGui.QImageReader.supportedImageFormats()]
-
-    print "Qt plugin paths: " + unicode(list(app.libraryPaths()))
-    print "Qt image read support: " + ', '.join(formats)
-    print 'Qt Libraries path: ' + unicode(QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibrariesPath))
-    label = QtGui.QLabel("Hello World from PySide", None)
+    app = QApplication(sys.argv)
+    label = QLabel("Hello World from PySide", None)
     label.setWindowTitle("Hello World from PySide")
     label.resize(300, 300)
     label.show()
