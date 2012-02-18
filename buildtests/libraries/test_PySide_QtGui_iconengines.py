a760c949fb6c7fbd7185bf4c8e4a6739410ca64b
Dave Elcock <dave.git.2012@coolhandmook.com>
2012-02-18 00:47:13 +0000

    Tweak test to explain why it might be failing.

--- buildtests/libraries/test_PySide_QtGui_iconengines.py
+++ buildtests/libraries/test_PySide_QtGui_iconengines.py
@@ -1,14 +1,21 @@
 import sys
+from PySide.QtCore import QSize
 from PySide.QtGui import QApplication, QIcon
 
 app = QApplication([])
 icon = QIcon('test_PySide_QtGui_iconengines.svg')
+pixmap = icon.pixmap(1,1)
+pixel = pixmap.toImage().pixel(0,0)
 
-if icon.pixmap(1,1).toImage().pixel(0,0) == 0xFF0000FF:
+if pixmap.size() != QSize(1, 1):
+    print "FAIL: Failed to convert to pixmap"
+    print "      Ensure your PySide install folder is first in your PATH"
+    status = 1
+elif pixel != 0xFF0000FF:
+    print "FAIL: pixel should be blue"
+    status = 1
+else:
     print "PASS: pixel was blue"
     status = 0
-else:
-    print "FAIL: pixel was not blue"
-    status = 1
 
 sys.exit(status)
